33d6a136ae131562096f722a99a6f8bfd592a520
LKMNHL <gi5138244@gmail.com>
2023-03-03 12:56:35 +0900

    test file upload

---
+++ webcam.py
@@ -0,0 +1,16 @@
+import cv2
+
+camera=cv2.VideoCapture(0)
+
+while True:
+    check, frame=camera.read()
+
+    cv2.imshow('Video', frame)
+
+    key=cv2.waitKey(1)
+
+    if key==27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
